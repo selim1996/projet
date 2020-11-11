7936f9e04f899777118ddfa963f6085e391b59e7
selim RAIS <selim.rais@esprit.tn>
2020-11-11 15:44:08 +0100

    commit

---
+++ src/ListStore.js
@@ -0,0 +1,31 @@
+import { observable, action, computed } from "mobx";
+import { v4 } from "uuid";
+
+export class List {
+  @observable value
+  @observable done
+
+  constructor (value) {
+    this.id = v4()
+    this.value = value
+  }
+}
+
+export class ListStore {
+  @observable lists = []
+  @observable filter = ""
+
+  @action addList = (value) => {
+    this.lists.push(new List(value))
+  }
+ 
+  @action deleteList = (list) => {
+    this.lists = this.lists.filter(t => t !== list)
+  }
+
+  @computed get filteredLists () {
+    console.log(this.lists)
+    const matchCase = new RegExp(this.filter, "i")
+    return this.lists.filter(list=> !this.filter || matchCase.test(list.value))
+  }
+}
